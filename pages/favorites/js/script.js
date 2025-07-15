8dda67156b841c4431f5108169f246e3a4309721
Eduardoamaro04 <eduamaro04@gmail.com>
2025-07-15 10:31:09 -0500

    feat: configurando o grid

--- pages/favorites/js/script.js
+++ pages/favorites/js/script.js
@@ -1,16 +1,22 @@
-//Modificador do Grid
 document.addEventListener('DOMContentLoaded', () => {
-    const gridBtn = document.querySelector('.ri-grid-fill');
-    const listBtn = document.querySelector('.ri-menu-line');
-    const caixaProdutos = document.querySelector('.caixa-produtos');
+  const gridBtn = document.querySelector('.ri-grid-fill');
+  const listBtn = document.querySelector('.ri-menu-line');
+  const caixaProdutos = document.querySelector('.caixa-produtos');
+  const produtos = document.querySelectorAll('.produto');
 
-    gridBtn.addEventListener('click', () => {
+  gridBtn.addEventListener('click', () => {
       caixaProdutos.classList.add('grid-view');
       caixaProdutos.classList.remove('list-view');
-    });
+      produtos.forEach(produto => {
+          produto.classList.add('compacto');
+      });
+  });
 
-    listBtn.addEventListener('click', () => {
+  listBtn.addEventListener('click', () => {
       caixaProdutos.classList.add('list-view');
       caixaProdutos.classList.remove('grid-view');
-    });
+      produtos.forEach(produto => {
+          produto.classList.remove('compacto');
+      });
   });
+});
